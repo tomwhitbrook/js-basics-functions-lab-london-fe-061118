14c58f1b58c86d552dada1ffedf85fcf248e4999
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-07-16 18:55:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ function calculatesFarePrice(startStreet, endStreet) {
   
   else if (dist > 2500) return "cannot travel that far";
   
-  else if (dist < 2001) return ((dist - 400) *
+  else if (dist < 2001) return ((dist - 400) * 0.02);
+  
+  else if (dist > 2000) return 25;
   
 }
