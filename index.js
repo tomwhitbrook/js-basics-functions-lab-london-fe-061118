3f948f333073fb36884a55225ff83657cd3b298c
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-07-16 19:06:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+  if  (streetNumber > 42) {
+    return (streetNumber - 42);
+  }
+  return (42 - streetNumber);
+}
+
+function distanceFromHqInFeet(streetNumber) {
+  let NoBlocks = distanceFromHqInBlocks(streetNumber);
+  
+  return (NoBlocks * 264)
+}
+
+function distanceTravelledInFeet(startStreet, endStreet) {
+  if (endStreet > startStreet) {
+  return ((endStreet - startStreet) * 264);
+  }
+  return ((startStreet - endStreet) * 264);
+}
+
+function calculatesFarePrice(startStreet, endStreet) {
+  let dist = distanceTravelledInFeet(startStreet, endStreet);
+  
+  if (dist < 400) return 0;
+  
+  else if (dist > 2500) return "cannot travel that far";
+  
+  else if (dist < 2001) return ((dist - 400) * 0.02);
+  
+  else if (dist > 2000) return 25;
+  
+}
