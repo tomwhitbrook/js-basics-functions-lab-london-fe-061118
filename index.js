b583220e93dd832e8f589676d8da88ee581ae0ef
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-07-16 18:11:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+  if  (streetNumber > 42) {
+    return (streetNumber - 42);
+  }
+  return (42 - streetNumber);
+}
+
+function distanceFromHqInFeet(streetNumber) {
+  distanceFromHqInBlocks(streetNumber);
+  return (streetNumber * 264)
+}
+
